331c573454134aa44ffce46c1235f599f6256283
Jasper-MultiSafepay <jasper.nadi@multisafepay.com>
2020-02-19 14:14:05 +0100

    Add mockclient on other places

--- tests/Api/TransactionsTest.php
+++ tests/Api/TransactionsTest.php
@@ -53,6 +53,18 @@ class TransactionsTest extends TestCase
      */
     public function testCreateTransactionWithInvalidApiKey(): void
     {
+        $mockClient = new MockClient();
+        $mockClient->addResponse(new Response(
+            401,
+            ['content-type' => 'application/json'],
+            json_encode([
+                'success' => false,
+                'data' => [],
+                'error_code' => 1032,
+                'error_info' => 'Invalid API key'
+            ])
+        ));
+
         $orderId = time();
         $orderData = [
             'type' => 'redirect',
@@ -62,7 +74,7 @@ class TransactionsTest extends TestCase
             'description' => 'Test transaction'
         ];
 
-        $multisafepay = new Api('__invalid__', false);
+        $multisafepay = new Api('__invalid__', false, $mockClient);
         $this->expectException(ApiException::class);
         $this->expectExceptionCode(1032);
         $this->expectExceptionMessage('Invalid API key');
@@ -74,7 +86,7 @@ class TransactionsTest extends TestCase
      */
     public function testGetTransactionWithValidApiKey(): void
     {
-        $orderId = strval(time());
+        $orderId = (string)time();
         $mockClient = new MockClient();
         $mockClient->addResponse(new Response(
             200,
@@ -98,12 +110,24 @@ class TransactionsTest extends TestCase
     }
 
     /**
-     * Test the return of an Exception when an invalid API key is being used.
+     * Test the return of an Exception when an invalid order Id is being used.
      */
     public function testGetTransactionWithInvalidOrderId(): void
     {
-        $orderId = strval(time());
-        $multisafepay = new Api('__invalid__', false);
+        $mockClient = new MockClient();
+        $mockClient->addResponse(new Response(
+            401,
+            ['content-type' => 'application/json'],
+            json_encode([
+                'success' => false,
+                'data' => [],
+                'error_code' => 1006,
+                'error_info' => 'Invalid transaction ID'
+            ])
+        ));
+
+        $orderId = (string)time();
+        $multisafepay = new Api('__invalid__', false, $mockClient);
         $this->expectException(ApiException::class);
         $this->expectExceptionCode(1006);
         $this->expectExceptionMessage('Invalid transaction ID');
