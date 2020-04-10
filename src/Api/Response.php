b8b57883079af64f518af8b9509bbd3619b22b97
Jisse Reitsma <jisse@yireo.com>
2020-04-10 15:50:04 +0200

    New Reponse object to be used elsewhere (#42)
    * New Reponse object to be used elsewhere
    
    * Shorten code
    
    * Update src/Api/Response.php

---
+++ src/Api/Response.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © 2019 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+namespace MultiSafepay\Api;
+
+use MultiSafepay\Exception\ApiException;
+
+/**
+ * Class Response
+ * @package MultiSafepay\Api
+ */
+class Response
+{
+    const ERROR_UNKNOWN_DATA = [412, 'Unknown data'];
+    const ERROR_INVALID_DATA_TYPE = [412, 'Invalid data type'];
+
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * Response constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        if (!isset($data['success']) && !empty($data['data'])) {
+            $data['success'] = true;
+        }
+
+        if (!isset($data['success'])) {
+            $data['success'] = false;
+        }
+
+        $this->validate($data);
+        $this->data = $data['data'];
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     * @throws ApiException
+     */
+    private function validate(array $data): bool
+    {
+        if ((bool)$data['success'] === true) {
+            return true;
+        }
+
+        $errorCode = $data['error_code'] ?? self::ERROR_UNKNOWN_DATA[0];
+        $errorInfo = $data['error_info'] ?? self::ERROR_UNKNOWN_DATA[1];
+
+        if (!empty($data['data']) && !is_array($data['data'])) {
+            list($errorCode, $errorInfo) = self::ERROR_INVALID_DATA_TYPE;
+        }
+
+        if (!$data['success']) {
+            throw new ApiException($errorInfo, $errorCode);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getResponseData(): array
+    {
+        return $this->data;
+    }
+}
---
+++ tests/Unit/Api/ResponseTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace MultiSafepay\Tests\Unit\Api;
+
+use MultiSafepay\Api\Response;
+use MultiSafepay\Exception\ApiException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ResponseTest
+ * @package MultiSafepay\Tests\Unit\Api
+ */
+class ResponseTest extends TestCase
+{
+    /**
+     * Test valid use cases
+     *
+     * @return void
+     */
+    public function testGetResponseData()
+    {
+        $response = new Response(['data' => ['foo' => 'bar'], 'success' => 1]);
+        $this->assertContains('bar', $response->getResponseData());
+
+        $response = new Response(['data' => ['foo' => 'bar']]);
+        $this->assertContains('bar', $response->getResponseData());
+
+        $response = new Response(['data' => [], 'success' => 1]);
+        $this->assertEmpty($response->getResponseData());
+    }
+
+    /**
+     * Test invalid use cases
+     *
+     * @return void
+     */
+    public function testGetResponseDataWithErrors()
+    {
+        $this->expectException(ApiException::class);
+
+        $this->expectExceptionCode(42);
+        $this->expectExceptionMessage('Sample error message');
+        new Response([
+            'data' => ['foo' => 'bar'],
+            'success' => 0,
+            'error_code' => 42,
+            'error_info' => 'Sample error message'
+        ]);
+
+        $this->expectExceptionCode(42);
+        $this->expectExceptionMessage('Sample error message');
+        new Response([
+            'data' => ['foo' => 'bar'],
+            'success' => 0,
+            'error_code' => 42,
+            'error_info' => 'Sample error message'
+        ]);
+
+        $this->expectExceptionCode(Response::ERROR_UNKNOWN_DATA[0]);
+        $this->expectExceptionMessage(Response::ERROR_UNKNOWN_DATA[1]);
+        new Response([]);
+
+        $this->expectExceptionCode(Response::ERROR_INVALID_DATA_TYPE[0]);
+        $this->expectExceptionMessage(Response::ERROR_INVALID_DATA_TYPE[1]);
+        new Response(['data' => []]);
+
+        $this->expectExceptionCode(Response::ERROR_INVALID_DATA_TYPE[0]);
+        $this->expectExceptionMessage(Response::ERROR_INVALID_DATA_TYPE[1]);
+        new Response(['data' => 'foobar', 'success' => 1]);
+
+        $this->expectExceptionCode(Response::ERROR_INVALID_DATA_TYPE[0]);
+        $this->expectExceptionMessage(Response::ERROR_INVALID_DATA_TYPE[1]);
+        new Response(['success' => 0]);
+    }
+}
