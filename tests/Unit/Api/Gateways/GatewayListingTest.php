73f5ea5820382f2c23a8aad9c7019f839c60fa2f
Jisse Reitsma <jisse@yireo.com>
2020-04-14 12:19:08 +0200

    Add missing tests

---
+++ tests/Unit/Api/Gateways/GatewayListingTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace MultiSafepay\Tests\Unit\Api\Gateways;
+
+use MultiSafepay\Api\Base\Response;
+use MultiSafepay\Api\Gateways\Gateway;
+use MultiSafepay\Api\Gateways\GatewayListing;
+use MultiSafepay\Exception\ApiException;
+use MultiSafepay\Exception\InvalidDataInitializationException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GatewayListingTest
+ * @package MultiSafepay\Tests\Unit\Api\Gateways
+ */
+class GatewayListingTest extends TestCase
+{
+    /**
+     * Test normal initialization
+     */
+    public function testGetGateways()
+    {
+        $gatewayListing = new GatewayListing([['id' => 'foo', 'description' => 'bar']]);
+        $gateways = $gatewayListing->getGateways();
+        $this->assertEquals(1, count($gateways));
+
+        $gateway = array_shift($gateways);
+        $this->assertEquals('foo', $gateway->getId());
+        $this->assertEquals('bar', $gateway->getDescription());
+    }
+}
