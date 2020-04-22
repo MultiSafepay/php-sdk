e1cdcdf2612b8175fcf9115ca006b9146d809628
Jisse Reitsma <jisse@yireo.com>
2020-04-22 15:48:23 +0200

    Work on ValueObjects for RequestOrders

---
+++ src/Api/Transactions/RequestOrder/GatewayInfo.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © 2020 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+namespace MultiSafepay\Api\Transactions\RequestOrder;
+
+/**
+ * Class GatewayInfo
+ * @package MultiSafepay\Api\Transactions\RequestOrder
+ */
+class GatewayInfo
+{
+    /**
+     * @var string
+     */
+    private $issuerId;
+
+    /**
+     * GatewayInfo constructor.
+     * @param string $issuerId
+     */
+    public function __construct(string $issuerId) {
+        $this->issuerId = $issuerId;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return [
+            'issuerId' => $this->issuerId,
+        ];
+    }
+}
---
+++ src/Api/Transactions/RequestOrder/GoogleAnalytics.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © 2020 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+namespace MultiSafepay\Api\Transactions\RequestOrder;
+
+/**
+ * Class GoogleAnalytics
+ * @package MultiSafepay\Api\Transactions\RequestOrder
+ */
+class GoogleAnalytics
+{
+    /**
+     * @var string
+     */
+    private $accountId;
+
+    /**
+     * GoogleAnalytics constructor.
+     * @param string $accountId
+     */
+    public function __construct(string $accountId) {
+        $this->accountId = $accountId;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return [
+            'account' => $this->accountId,
+        ];
+    }
+}
