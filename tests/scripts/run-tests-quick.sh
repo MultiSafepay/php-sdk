ef6b310e927017f6910b527d8bc6f007a45f13a0
jisse Reitsma <jisse@yireo.com>
2020-06-03 17:02:05 +0200

    Better output checking

--- tests/scripts/run-tests-quick.sh
+++ tests/scripts/run-tests-quick.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-vendor/bin/phpcs --standard=phpcs.ruleset.xml .
-vendor/bin/phpunit tests/Unit
-vendor/bin/phpunit tests/Integration
-vendor/bin/phpstan analyse src/ tests/ --level 1
+vendor/bin/phpcs --standard=phpcs.ruleset.xml . || exit
+vendor/bin/phpunit tests/Unit || exit
+vendor/bin/phpunit tests/Integration || exit
+vendor/bin/phpstan analyse --no-progress src/ tests/ --level 1
