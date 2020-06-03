ef057ce80751c8f2caa399700f8405f520d5bd8f
jisse Reitsma <jisse@yireo.com>
2020-06-03 17:02:32 +0200

    Better output checking

--- tests/scripts/run-tests-full.sh
+++ tests/scripts/run-tests-full.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-vendor/bin/phpcs --standard=phpcs.ruleset.xml .
-vendor/bin/phpunit tests
-vendor/bin/phpstan analyse src/ tests/ --level 1
+vendor/bin/phpcs --standard=phpcs.ruleset.xml . || exit
+vendor/bin/phpunit tests || exit
+vendor/bin/phpstan analyse src/ tests/ --level 1 || exit
--- tests/scripts/run-tests-quick.sh
+++ tests/scripts/run-tests-quick.sh
@@ -2,4 +2,4 @@
 vendor/bin/phpcs --standard=phpcs.ruleset.xml . || exit
 vendor/bin/phpunit tests/Unit || exit
 vendor/bin/phpunit tests/Integration || exit
-vendor/bin/phpstan analyse --no-progress src/ tests/ --level 1
+vendor/bin/phpstan analyse --no-progress src/ tests/ --level 1 || exit
