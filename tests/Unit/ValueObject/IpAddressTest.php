a8ed0f0394a206c13151f0372bc0afc99118ac11
Daniel Civit <daniel.civit@multisafepay.com>
2025-02-13 08:39:36 +0100

    PHPSDK-168: Adding unit tests to cover IPv6 addresses over the IpAddress object (#374)

--- tests/Unit/ValueObject/IpAddressTest.php
+++ tests/Unit/ValueObject/IpAddressTest.php
@@ -12,12 +12,40 @@ use PHPUnit\Framework\TestCase;
 class IpAddressTest extends TestCase
 {
     /**
-     * Test whether a value could be set and used
+     * Test whether a valid IPv4 address can could be set and used
      */
     public function testWhetherValueCanBeSetAndUsed()
     {
         $ipAddress = new IpAddress('10.0.0.1');
         $this->assertEquals('10.0.0.1', $ipAddress->get());
+    }
+
+    /**
+     * Test if the first valid IPv4 address from a comma-separated list can be set and retrieved.
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For#examples
+     */
+    public function testWhetherCommaSeparatedValuesCanBeSetAndUsed()
+    {
+        $ipAddress = new IpAddress('203.0.113.195, 70.41.3.18, 150.172.238.178');
+        $this->assertEquals('203.0.113.195', $ipAddress->get());
+    }
+
+    /**
+     * Test if a valid IPv6 address can be set and retrieved.
+     */
+    public function testWhetherIPv6ValueCanBeSetAndUsed()
+    {
+        $ipAddress = new IpAddress('2001:0db8:85a3:0000:0000:8a2e:0370:7334');
+        $this->assertEquals('2001:0db8:85a3:0000:0000:8a2e:0370:7334', $ipAddress->get());
+    }
+
+    /**
+     * Test if the first valid IPv6 address from a comma-separated list can be set and retrieved.
+     */
+    public function testWhetherCommaSeparatedIPv6ValuesCanBeSetAndUsed()
+    {
+        $ipAddress = new IpAddress('2001:0db8:85a3:0000:0000:8a2e:0370:7334, 2001:0db8:85a3:0000:0000:8a2e:0370:7335');
+        $this->assertEquals('2001:0db8:85a3:0000:0000:8a2e:0370:7334', $ipAddress->get());
     }
 
     /**
@@ -27,15 +55,5 @@ class IpAddressTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
         new IpAddress('foobar');
-    }
-
-    /**
-     * Test whether comma-separated values could be set and used
-     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For#examples
-     */
-    public function testWhetherCommaSeparatedValuesCanBeSetAndUsed()
-    {
-        $ipAddress = new IpAddress('203.0.113.195, 70.41.3.18, 150.172.238.178');
-        $this->assertEquals('203.0.113.195', $ipAddress->get());
     }
 }
